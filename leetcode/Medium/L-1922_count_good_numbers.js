7cf5c9660871487080d69fa3f486465627a28756
Md-Isa-Ahamed-San <glordisagtav@gmail.com>
2024-08-26 22:19:30 +0600

    'gg'

--- leetcode/Medium/L-1922_count_good_numbers.js
+++ leetcode/Medium/L-1922_count_good_numbers.js
@@ -9,9 +9,12 @@ var countGoodNumbers = function (n) {
     while (exp > 0n) {
       if (exp % 2n === 1n) {
         result = (result * base) % mod;
+        exp= exp-1n;
       }
-      exp = exp / 2n;
-      base = (base * base) % mod;
+      else{
+        base = (base * base) % mod;
+        exp = exp / 2n;
+      }
     }
     return result;
   }
@@ -23,4 +26,10 @@ var countGoodNumbers = function (n) {
   let oddResult = modPow(4n, oddPower, MOD);
 
   return ((evenResult * oddResult) % MOD).toString();
+
 };
+//here suppose the input is 4 in that case index will be 0,1,2,3,4 here the ans 
+//will be 5^2(for even index) * 4^2(for odd index). which can be written as
+// 5^n/2 * 5^n/2 which is equal to 20^n/2. so basically the ans will be 
+//20^n/2 of even input and 20^n/2 * 5 for odd input. so we can do this also
+//to solve this problem.
