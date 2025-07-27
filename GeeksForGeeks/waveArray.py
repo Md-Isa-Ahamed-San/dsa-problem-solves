3fa139da5f5472b22c214d334093790b19a899cf
Md-Isa-Ahamed-San <glordisagtav@gmail.com>
2025-07-27 21:31:34 +0600

    solved wave array problem from geeks for geeks

---
+++ GeeksForGeeks/waveArray.py
@@ -0,0 +1,11 @@
+def sortInWave(arr):
+        n = len(arr)
+        for i in range(0, n-1, 2):
+                arr[i],arr[i+1] = arr[i+1], arr[i]
+        print(arr)
+        return arr
+
+sortInWave([1, 2, 3, 4, 5, 6, 7, 8])
+sortInWave([2, 4, 7, 8, 9, 10])
+sortInWave([2, 4, 7, 8, 9])
+sortInWave([1])
