254914293214699c2f16aa1a109e9bc11b692f42
Md-Isa-Ahamed-San <glordisagtav@gmail.com>
2025-12-29 14:57:29 +0600

    a linelast mail

---
+++ codeforces/A_Lineland_Mail.py
@@ -0,0 +1,133 @@
+# -------------------------------
+#  FAST PYTHON CP TEMPLATE
+# -------------------------------
+
+import math
+import sys
+import threading
+
+input = sys.stdin.readline
+
+# ---------- CONSTANTS ----------
+MOD = 10**9 + 7
+INF = 10**18
+
+
+# ---------- BASIC IO ----------
+def integers_input():
+    return map(int, input().split())
+
+
+def list_input():
+    return list(map(int, input().split()))
+
+
+def simple_input():
+    return input().strip()
+
+
+# ---------- COMMON PRINTS ----------
+def yes():
+    print("YES")
+
+
+def no():
+    print("NO")
+
+
+def print_arr(arr):
+    print(*arr)
+
+
+# ---------- MATH UTILS ----------
+def is_even(x):
+    return x & 1 == 0
+
+
+def is_odd(x):
+    return x & 1 == 1
+
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    return a // gcd(a, b) * b
+
+
+def ceil_div(a, b):
+    return (a + b - 1) // b
+
+
+# ---------- ARRAY UTILS ----------
+def prefix_sum(arr):
+    n = len(arr)
+    ps = [0] * (n + 1)
+    for i in range(n):
+        ps[i + 1] = ps[i] + arr[i]
+    return ps
+
+
+def suffix_sum(arr):
+    n = len(arr)
+    ss = [0] * (n + 1)
+    for i in range(n - 1, -1, -1):
+        ss[i] = ss[i + 1] + arr[i]
+    return ss
+
+
+# ---------- BINARY SEARCH ----------
+def lower_bound(arr, x):
+    l, r = 0, len(arr)
+    while l < r:
+        m = (l + r) // 2
+        if arr[m] < x:
+            l = m + 1
+        else:
+            r = m
+    return l
+
+
+def upper_bound(arr, x):
+    l, r = 0, len(arr)
+    while l < r:
+        m = (l + r) // 2
+        if arr[m] <= x:
+            l = m + 1
+        else:
+            r = m
+    return l
+
+
+# ---------- SOLVE ----------
+def solve():
+    n = int(input())
+    arr = list(map(int, input().strip().split()))
+    ans_pair_arr = []
+
+    for i in range(n):
+        if i == 0:
+            min_val = arr[1] - arr[0]
+        elif i == n - 1:
+            min_val = arr[n - 1] - arr[n - 2]
+        else:
+            min_val = min(arr[i] - arr[i - 1], arr[i + 1] - arr[i])
+
+        max_val = max(arr[i] - arr[0], arr[n - 1] - arr[i])
+
+        ans_pair_arr.append((min_val, max_val))
+
+    for a, b in ans_pair_arr:
+        print(a, b)
+
+
+# ---------- MAIN ----------
+def main():
+    solve()
+
+
+if __name__ == "__main__":
+    main()
