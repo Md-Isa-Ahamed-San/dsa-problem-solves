5a4938ba3aba8132dd1c07d3d9dab0c6e2b8289c
Md-Isa-Ahamed-San <glordisagtav@gmail.com>
2025-12-29 12:27:02 +0600

    solved A team olympiad

---
+++ codeforces/A_Team_Olympiad.py
@@ -0,0 +1,112 @@
+# -------------------------------
+#  FAST PYTHON CP TEMPLATE
+# -------------------------------
+
+import math
+import sys
+import threading
+input = sys.stdin.readline
+
+# ---------- CONSTANTS ----------
+MOD = 10**9 + 7
+INF = 10**18
+
+# ---------- BASIC IO ----------
+def integers_input():
+    return map(int, input().split())
+
+def list_input():
+    return list(map(int, input().split()))
+
+def simple_input():
+    return input().strip()
+
+# ---------- COMMON PRINTS ----------
+def yes():
+    print("YES")
+
+def no():
+    print("NO")
+
+def print_arr(arr):
+    print(*arr)
+
+# ---------- MATH UTILS ----------
+def is_even(x):
+    return x & 1 == 0
+
+def is_odd(x):
+    return x & 1 == 1
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a // gcd(a, b) * b
+
+def ceil_div(a, b):
+    return (a + b - 1) // b
+
+# ---------- ARRAY UTILS ----------
+def prefix_sum(arr):
+    n = len(arr)
+    ps = [0] * (n + 1)
+    for i in range(n):
+        ps[i + 1] = ps[i] + arr[i]
+    return ps
+
+def suffix_sum(arr):
+    n = len(arr)
+    ss = [0] * (n + 1)
+    for i in range(n - 1, -1, -1):
+        ss[i] = ss[i + 1] + arr[i]
+    return ss
+
+# ---------- BINARY SEARCH ----------
+def lower_bound(arr, x):
+    l, r = 0, len(arr)
+    while l < r:
+        m = (l + r) // 2
+        if arr[m] < x:
+            l = m + 1
+        else:
+            r = m
+    return l
+
+def upper_bound(arr, x):
+    l, r = 0, len(arr)
+    while l < r:
+        m = (l + r) // 2
+        if arr[m] <= x:
+            l = m + 1
+        else:
+            r = m
+    return l
+
+# ---------- SOLVE ----------
+def solve():
+    n = int(input().strip())
+    arr = list(map(int,input().strip().split()))
+    idx_arr_1 =[]
+    idx_arr_2 =[]
+    idx_arr_3 = []
+    for idx,val in enumerate(arr):
+        if val == 1:
+            idx_arr_1.append(idx)
+        elif val == 2:
+            idx_arr_2.append(idx)
+        elif val == 3:
+            idx_arr_3.append(idx)
+    min_val = min(len(idx_arr_1), len(idx_arr_2), len(idx_arr_3))
+    print(min_val)
+    for i in range(min_val):
+        print(idx_arr_1[i]+1,idx_arr_2[i]+1,idx_arr_3[i]+1)
+
+# ---------- MAIN ----------
+def main():
+    solve()
+
+if __name__ == "__main__":
+    main()
