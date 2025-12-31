a937ef0e7309a50c13d81dccb8f6f81d57a8dd02
Md-Isa-Ahamed-San <glordisagtav@gmail.com>
2025-12-31 21:57:36 +0600

    solved codechef contest pb

--- codeforces/A_Counterexample.py
+++ codeforces/A_Counterexample.py
@@ -3,7 +3,6 @@
 # -------------------------------
 
 import sys
-import math
 
 input = sys.stdin.readline
 
@@ -11,43 +10,55 @@ input = sys.stdin.readline
 MOD = 10**9 + 7
 INF = 10**18
 
+
 # ---------- BASIC IO ----------
 def ints():
     return map(int, input().split())
 
+
 def list_ints():
     return list(map(int, input().split()))
 
+
 def str_input():
     return input().strip()
+
 
 # ---------- COMMON OUTPUT ----------
 def yes():
     print("YES")
 
+
 def no():
     print("NO")
 
+
 def out(arr):
     print(*arr)
+
 
 # ---------- MATH UTILS ----------
 def is_even(x):
     return (x & 1) == 0
 
+
 def is_odd(x):
     return (x & 1) == 1
+
 
 def gcd(a, b):
     while b:
         a, b = b, a % b
     return a
 
+
 def lcm(a, b):
     return a // gcd(a, b) * b
 
+
 def ceil_div(a, b):
     return (a + b - 1) // b
+
 
 # ---------- PRIME CHECK (NORMAL) ----------
 def is_prime(n):
@@ -62,6 +73,7 @@ def is_prime(n):
             return False
     return True
 
+
 # ---------- SIEVE OF ERATOSTHENES ----------
 def is_prime_sieve(n):
     is_prime_arr = [True] * (n + 1)
@@ -72,6 +84,7 @@ def is_prime_sieve(n):
                 is_prime_arr[j] = False
     return is_prime_arr
 
+
 # ---------- PREFIX / SUFFIX ----------
 def prefix_sum(arr):
     ps = [0]
@@ -79,11 +92,13 @@ def prefix_sum(arr):
         ps.append(ps[-1] + x)
     return ps
 
+
 def suffix_sum(arr):
     ss = [0] * (len(arr) + 1)
     for i in range(len(arr) - 1, -1, -1):
         ss[i] = ss[i + 1] + arr[i]
     return ss
+
 
 # ---------- BINARY SEARCH ----------
 def lower_bound(arr, x):
@@ -96,6 +111,7 @@ def lower_bound(arr, x):
             r = m
     return l
 
+
 def upper_bound(arr, x):
     l, r = 0, len(arr)
     while l < r:
@@ -106,19 +122,22 @@ def upper_bound(arr, x):
             r = m
     return l
 
+
 # ---------- SOLVE ----------
 def solve():
-    l,r = map(int,input().strip().split())
-    for i in range(l,r-1,1):
-        #print(i)
-        if gcd(i,i+1) == 1 and gcd(i+1,i+2)==1 and gcd(i,i+2)!=1:
-            print(i,i+1,i+2)
+    l, r = map(int, input().strip().split())
+    for i in range(l, r - 1, 1):
+        # print(i)
+        if gcd(i, i + 1) == 1 and gcd(i + 1, i + 2) == 1 and gcd(i, i + 2) != 1:
+            print(i, i + 1, i + 2)
             return
     print(-1)
+
 
 # ---------- MAIN ----------
 def main():
     solve()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
