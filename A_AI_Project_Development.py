f02ac13623e71a4d89d84f4e3c3234127c07c1e3
Md-Isa-Ahamed-San <glordisagtav@gmail.com>
2026-09-01 17:15:30 +0600

    solved an easy 800 problem

---
+++ A_AI_Project_Development.py
@@ -0,0 +1,128 @@
+# --------------------------------
+#  Problem : A_AI_Project_Development
+#  Rating  : auto from Codeforces
+#  Tags    : auto from Codeforces
+#  Author  : Md Isa Ahamed San
+#  Date    : 2026-09-01
+# --------------------------------
+
+# region TEMPLATE
+
+import sys
+import math
+from collections import defaultdict, deque, Counter
+from itertools import accumulate
+import heapq
+
+# ---------- FAST IO / PyPy Optimization ----------
+input = sys.stdin.readline
+# import os; input = lambda: os.read(0, 8192).decode().rstrip()
+
+# ---------- CONSTANTS ----------
+MOD = 10**9 + 7
+INF = 10**18
+
+
+# ---------- BASIC IO ----------
+def ints():
+    return map(int, input().split())
+
+
+def list_ints():
+    return list(map(int, input().split()))
+
+
+def str_input():
+    return input().strip()
+
+
+# ---------- COMMON OUTPUT ----------
+def yes():
+    print("YES")
+
+
+def no():
+    print("NO")
+
+
+def out(arr):
+    print(*arr)
+
+
+# ---------- MATH UTILS ----------
+def is_even(n):
+    return (n & 1) == 0
+
+
+def is_odd(n):
+    return (n & 1) == 1
+
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    return a // gcd(a, b) * b
+
+
+def ceil_div(a, b):
+    return (a + b - 1) // b
+
+
+# ---------- PRIME CHECK ----------
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for d in range(3, int(n**0.5) + 1, 2):
+        if n % d == 0:
+            return False
+    return True
+
+
+# ---------- PREFIX / SUFFIX ----------
+def prefix_sum(arr):
+    pre = [0]
+    for x in arr:
+        pre.append(pre[-1] + x)
+    return pre
+
+
+def suffix_sum(arr):
+    n = len(arr)
+    suf = [0] * (n + 1)
+    for i in range(n - 1, -1, -1):
+        suf[i] = suf[i + 1] + arr[i]
+    return suf
+
+
+# endregion
+
+
+# ---------- SOLVE ----------
+def solve():
+    t = int(input().strip())
+    for _ in range(t):
+        n, x, y, z = map(int, input().strip().split())
+
+        count_1 = math.ceil(n / (x + y))
+        count_2=z
+        n=n-(z*x)
+        b = math.ceil(n / (x + (10 * y)))
+        count_2+=b
+        print(min(count_1,count_2))
+
+
+# ---------- MAIN ----------
+def main():
+    solve()
+
+
+if __name__ == "__main__":
+    main()
---
+++ B_Merge_to_Match.py
@@ -0,0 +1,111 @@
+# --------------------------------
+#  Problem : B_Merge_to_Match
+#  Rating  : auto from Codeforces
+#  Tags    : auto from Codeforces
+#  Author  : Md Isa Ahamed San
+#  Date    : 2026-08-30
+# --------------------------------
+
+# region TEMPLATE
+
+import sys
+import math
+from collections import defaultdict, deque, Counter
+from itertools import accumulate
+import heapq
+
+# ---------- FAST IO / PyPy Optimization ----------
+input = sys.stdin.readline
+# import os; input = lambda: os.read(0, 8192).decode().rstrip()
+
+# ---------- CONSTANTS ----------
+MOD = 10**9 + 7
+INF = 10**18
+
+# ---------- BASIC IO ----------
+def ints(): return map(int, input().split())
+def list_ints(): return list(map(int, input().split()))
+def str_input(): return input().strip()
+
+# ---------- COMMON OUTPUT ----------
+def yes(): print('YES')
+def no(): print('NO')
+def out(arr): print(*arr)
+
+# ---------- MATH UTILS ----------
+def is_even(n): return (n & 1) == 0
+def is_odd(n): return (n & 1) == 1
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a // gcd(a, b) * b
+
+def ceil_div(a, b):
+    return (a + b - 1) // b
+
+# ---------- PRIME CHECK ----------
+def is_prime(n):
+    if n < 2: return False
+    if n == 2: return True
+    if n % 2 == 0: return False
+    for d in range(3, int(n**0.5)+1, 2):
+        if n % d == 0: return False
+    return True
+
+# ---------- PREFIX / SUFFIX ----------
+def prefix_sum(arr):
+    pre = [0]
+    for x in arr:
+        pre.append(pre[-1] + x)
+    return pre
+
+def suffix_sum(arr):
+    n = len(arr)
+    suf = [0] * (n + 1)
+    for i in range(n - 1, -1, -1):
+        suf[i] = suf[i + 1] + arr[i]
+    return suf
+
+# endregion
+
+# ---------- SOLVE ----------
+def solve():
+    t= int(input().strip())
+    for _ in range(t):
+        n,m = map(int,input().strip().split())
+        a=list_ints()
+        b=list_ints()
+        is_yes=True
+        if len(a)<len(b)*2:
+            no()
+            continue
+        a.sort()
+        b.sort()
+        #left side check
+        for i in range(len(b)):
+            if b[i]<a[i]:
+                is_yes=False
+                break
+        #right side check
+        for i in range(len(b)-1,-1,-1):
+            if a[n-1-i]<b[m-1-i]:
+                is_yes=False
+                break
+        if is_yes:
+            yes()
+        else:
+            no()
+
+
+        
+
+# ---------- MAIN ----------
+def main():
+    solve()
+
+if __name__ == '__main__':
+    main()
